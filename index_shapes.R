f0700df85a3c9f9aaf491db4c4b87f5cb2221ac6
Pieter Provoost <pieterprovoost@gmail.com>
2023-12-28 22:41:57 +0100

    shape indexing

---
+++ index_shapes.R
@@ -0,0 +1,28 @@
+library(sf)
+library(dplyr)
+library(h3jsr)
+library(purrr)
+
+h3_res <- 7
+shapes <- read_sf("shapes/Navigator_Global_121923/Navigator_Global_121923.shp")
+
+st <- storr::storr_rds("shapes_storr")
+
+polygon_to_cells_possibly = possibly(.f = h3jsr::polygon_to_cells, otherwise = NULL)
+
+# TODO: check errors!
+
+walk(seq_along(shapes$SITE_ID), function(i) {
+  site_id <- shapes$SITE_ID[i]
+  if (!st$exists(site_id)) {
+    suppressMessages({
+      result <- polygon_to_cells_possibly(shapes$geometry[i], h3_res)      
+    })
+    if (!is.null(result)) {
+      cells <- result[[1]]
+      st$set(site_id, cells)
+    }
+  }
+}, .progress = TRUE)
+
+# st_remove_holes()
