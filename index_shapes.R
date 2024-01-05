e33049810abd70152a034685483167e214909828
Pieter Provoost <pieterprovoost@gmail.com>
2024-01-05 17:11:09 +0100

    navigator info

--- index_shapes.R
+++ index_shapes.R
@@ -2,19 +2,22 @@ library(sf)
 library(dplyr)
 library(h3jsr)
 library(purrr)
+library(glue)
 
 h3_res <- 7
+layers <- st_layers("shapes/Navigator_Global_121923/Navigator_Global_121923.shp")$name
 shapes <- read_sf("shapes/Navigator_Global_121923/Navigator_Global_121923.shp")
 
 st <- storr::storr_rds("shapes_storr")
 
 polygon_to_cells_possibly = possibly(.f = h3jsr::polygon_to_cells, otherwise = NULL)
 
-# TODO: check errors!
-
-walk(seq_along(shapes$SITE_ID), function(i) {
+for (i in 1:nrow(shapes)) {
   site_id <- shapes$SITE_ID[i]
-  if (!st$exists(site_id)) {
+  message(glue("{site_id} | {i}/{nrow(shapes)}"))
+  error_file <- glue("shapes_errors/{site_id}.txt")
+
+  if (!st$exists(site_id) & !file.exists(error_file)) {
     suppressMessages({
       result <- polygon_to_cells_possibly(shapes$geometry[i], h3_res)      
     })
@@ -22,9 +25,9 @@ walk(seq_along(shapes$SITE_ID), function(i) {
       cells <- result[[1]]
       st$set(site_id, cells)
     } else {
-      file.create(glue::glue("shapes_errors/{site_id}.txt"))
+      file.create(error_file)
     }
   }
-}, .progress = TRUE)
+}
 
 # st_remove_holes()
---
+++ navigator_info.R
@@ -0,0 +1,13 @@
+library(dplyr)
+
+info <- read.csv("../protectedseas/shapes/Navigator_Global_121923.csv")
+
+info %>%
+  group_by(designation) %>%
+  summarize(n = n()) %>%
+  arrange(desc(n))
+
+info %>%
+  group_by(category_name) %>%
+  summarize(n = n()) %>%
+  arrange(desc(n))
