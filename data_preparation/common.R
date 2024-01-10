b2d8707c1ee6110ae81111f74bb118d71aaabdad
Pieter Provoost <pieterprovoost@gmail.com>
2024-01-10 19:27:19 +0100

    restructuring

--- data_preparation/common.R
+++ data_preparation/common.R
@@ -3,7 +3,7 @@ h3_res <- 7
 gbif_snapshot_path <- "/Volumes/LaCie/gbif/occurrence.parquet/"
 obis_snapshot_path <- "/Volumes/LaCie/gbif/obis_20231025.parquet"
 
-sqlite_file <- "~/Desktop/database.sqlite"
+sqlite_file <- "database.sqlite"
 site_cells_table <- "site_cells"
 gbif_occurrence_table <- "gbif_occurrence"
 obis_occurrence_table <- "obis_occurrence"
